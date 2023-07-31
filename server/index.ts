bbe29ba632fa9075274fe70c611fde2deffc7c11
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-31 17:29:45 +0300

    feat (index.ts): добавлен index.ts

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import {sequelize} from './database/config';
+import dotenv from 'dotenv';
+import router from './routes';
+import errorHandler from './middleware/errorHandlingMiddleware';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+app.use(errorHandler);
+
+const start = async (): Promise<void> => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {});
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
