4e8b7f7edf035969de1a862fee213c633ece298e
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-08-03 03:59:12 +0300

    fix (server/index.ts): исправлен порт

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ import errorHandler from './middleware/errorHandlingMiddleware';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
