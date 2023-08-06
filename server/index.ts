47e26d9b94949ae6bab6a13a6e8f0c5e86847a3a
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-08-07 01:10:29 +0300

    fix (server/index.ts): исправлен index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,9 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import {sequelize} from './db/config';
-import dotenv from 'dotenv';
 import router from './routes';
 import errorHandler from './middleware/errorHandlingMiddleware';
-
-dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
@@ -13,17 +11,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/', router);
+app.use('/api', router);
 app.use(errorHandler);
 
-const start = async (): Promise<void> => {
+(async (): Promise<void> => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    app.listen(PORT, () => {});
+    app.listen(PORT, () => {
+      console.info(`Server started on ${PORT}`);
+    });
   } catch (error) {
     console.error(error);
   }
-};
-
-start();
+})();
